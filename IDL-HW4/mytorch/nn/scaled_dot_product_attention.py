4a1ea3182097cfec82561a7220b13ce0900be3e7
parth <parthg@andrew.cmu.edu>
2025-11-28 16:28:58 -0500

    Implement and test scaled dot product attention

--- IDL-HW4/mytorch/nn/scaled_dot_product_attention.py
+++ IDL-HW4/mytorch/nn/scaled_dot_product_attention.py
@@ -1,20 +1,21 @@
 import numpy as np
 from .activation import Softmax
 
+
 class ScaledDotProductAttention:
     """
     Scaled Dot Product Attention
-    """ 
+    """
+
     def __init__(self):
-        '''
+        """
         Initialize the ScaledDotProductAttention class.
-        '''
+        """
         # Initialize your softmax layer
         # What dimension should you pass to the softmax constructor?
-        self.eps = 1e10 # DO NOT MODIFY
-        self.softmax = NotImplementedError
-        
-    
+        self.eps = 1e10  # DO NOT MODIFY
+        self.softmax = Softmax(dim=-1)
+
     def forward(self, Q, K, V, mask=None):
         """
         :param Q: Query matrix of shape (N, ..., H, L, E) where L is target sequence length
@@ -24,26 +25,31 @@ class ScaledDotProductAttention:
         :return: Output matrix of shape (N, ..., H, L, Ev)
         """
         # TODO: Implement forward pass
-        
+        self.V = V
+        self.K = K
+        self.Q = Q
+
         # Calculate attention scores: (N, ..., H, L, S)
         # (N, ..., H, L, E) @ (N, ..., H, E, S) -> (N, ..., H, L, S)
-        scaled_dot_product = NotImplementedError
-        
+        dot_product = Q @ K.swapaxes(-2, -1)
+        self.d_k = Q.shape[-1]
+        scaled_dot_product = dot_product / np.sqrt(self.d_k)
+
         # Apply mask before softmax if provided
         # If mask is not None, add -self.eps to the attention scores for positions to ignore
         if mask is not None:
-            scaled_dot_product = NotImplementedError
+            scaled_dot_product = scaled_dot_product + (-self.eps) * mask
 
         # Compute attention scores: Apply softmax along S dimension (N, ..., H, L, S)
-        self.attention_scores = NotImplementedError
+        self.attention_scores = self.softmax.forward(scaled_dot_product)
 
         # Calculate output: (N, ..., H, L, Ev)
-        # (N, ..., H, L, S) @ (N, ..., H, S, Ev) -> (N, ..., H, L, Ev) 
-        output = NotImplementedError
+        # (N, ..., H, L, S) @ (N, ..., H, S, Ev) -> (N, ..., H, L, Ev)
+        output = self.attention_scores @ self.V
 
         # Return output
-        raise NotImplementedError
-    
+        return output
+
     def backward(self, d_output):
         """
         :param d_output: Gradient of loss wrt output of shape (N, ..., H, L, Ev)
@@ -52,24 +58,23 @@ class ScaledDotProductAttention:
         # TODO: Implement backward pass
 
         # Calculate gradients for V: (N, ..., H, S, Ev)
-        # (N, ..., H, L, S) @ (N, ..., H, S, Ev) -> (N, ..., H, L, Ev) 
-        # Use the transpose of stored softmax output to swap last two dimensions   
-        d_V = NotImplementedError
-        
+        # (N, ..., H, L, S) @ (N, ..., H, S, Ev) -> (N, ..., H, L, Ev)
+        # Use the transpose of stored softmax output to swap last two dimensions
+        d_V = self.attention_scores.swapaxes(-2, -1) @ d_output
+
         # Calculate gradients for attention scores
         # (N, ..., H, L, Ev) @ (N, ..., H, Ev, S) -> (N, ..., H, L, S)
-        d_attention_scores = NotImplementedError
-        d_scaled_dot_product = NotImplementedError
-        
-        # Scale gradients by sqrt(d_k)
-        d_scaled_dot_product = NotImplementedError
-        
-        # Calculate gradients for Q and K
-        # (N, ..., H, L, S) @ (N, ..., H, S, E) -> (N, ..., H, L, E)   
-        d_Q = NotImplementedError
-        # (N, ..., H, L, S) @ (N, ..., H, L, E) -> (N, ..., H, S, E)
-        d_K = NotImplementedError
-        
-        # Return gradients for Q, K, V
-        raise NotImplementedError
+        d_attention_scores = d_output @ self.V.swapaxes(-2, -1)
+        d_scaled_dot_product = self.softmax.backward(d_attention_scores)
 
+        # Scale gradients by sqrt(d_k)
+        d_scaled_dot_product = d_scaled_dot_product / np.sqrt(self.d_k)
+
+        # Calculate gradients for Q and K
+        # (N, ..., H, L, S) @ (N, ..., H, S, E) -> (N, ..., H, L, E)
+        d_Q = d_scaled_dot_product @ self.K
+        # (N, ..., H, L, S) @ (N, ..., H, L, E) -> (N, ..., H, S, E)
+        d_K = d_scaled_dot_product.swapaxes(-2, -1) @ self.Q
+
+        # Return gradients for Q, K, V
+        return d_Q, d_K, d_V
